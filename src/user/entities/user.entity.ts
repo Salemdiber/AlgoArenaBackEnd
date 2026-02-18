0448979876fd37a0eb2b61b5da3931a968744f79
Draouil Rayssen <131086403+rayssen-draouil@users.noreply.github.com>
2026-02-18 01:03:20 +0100

    user crud amelioration

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -2,10 +2,39 @@ import {
   Entity,
   Column,
   ObjectIdColumn,
-  BeforeInsert,
 } from 'typeorm';
 
+export enum Role {
+  Player = 'Player',
+  Admin = 'Admin',
+}
 @Entity()
 export class User {
+  @ObjectIdColumn()
+  userId: string;
 
-  }
+  @Column()
+  username: string;
+
+  @Column()
+  passwordHash: string;
+
+  @Column()
+  email: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.Player,
+  })
+  role: Role;
+
+  @Column({ nullable: true })
+  avatar: string | null;
+
+  @Column({ nullable: true })
+  bio: string | null;
+
+  @Column({ default: true })
+  status: boolean;
+}
